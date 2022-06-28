9bdef986a951d07b5d9dbac300e998278af35409
Oghenero Paul-Ejukorlem <nerocodes@Ogheneros-MacBook-Pro.local>
2022-06-28 08:03:21 +0100

    removed vue logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,8 +7,6 @@ import './assets/tailwind.css'
 <template>
   <div>
     <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
       <div class="wrapper">
         <Logo msg="Sub Manager" />
 
